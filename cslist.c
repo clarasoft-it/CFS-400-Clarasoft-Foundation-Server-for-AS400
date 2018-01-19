b143d567e72a016aded768542041cbb6f7f74bc6
Clarasoft I.T. Solutions Inc <fredericsoucie@clarasoft-it.com>
2018-01-19 13:04:22 +0000

    Update cslist.c

--- cslist.c
+++ cslist.c
@@ -319,7 +319,7 @@ CSRESULT CSLIST_Remove(CSLIST* This, long index) {
 
   if (This->numItems > 0)
   {
-    if (This->numItems == 0)
+    if (This->numItems == 1)
     {
       Temp = This->first;
 
