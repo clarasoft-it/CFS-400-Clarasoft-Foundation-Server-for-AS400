60da40395e6000c4163c5e1d9fd5230ba2a5f64a
Clarasoft I.T. Solutions Inc <fredericsoucie@clarasoft-it.com>
2018-02-05 14:12:21 +0000

    Update csstr.h
    Changed function definition of CSSTRCV_SetConversion; added more functionalities

--- csstr.h
+++ csstr.h
@@ -1,13 +1,10 @@
-
 /* ==========================================================================
-
   Clarasoft Core Tools
 
   csstr.h
   string utilities
+
   Version 1.0.0
-
-
 
   Distributed under the MIT license
 
@@ -20,10 +17,8 @@
   merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
-
   The above copyright notice and this permission notice shall be
   included in all copies or substantial portions of the Software.
-
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -31,19 +26,30 @@
   ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
   THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
 ========================================================================== */
 
-#ifndef __CSSTR_H__
-#define __CSSTR_H__
+#ifndef __CLARASOFT_CT_CSSTR_H__
+#define __CLARASOFT_CT_CSSTR_H__
 
 #include "qcsrc/cscore.h"
 
-#define CCSID_JOBDEFAULT   0
-#define CCSID_UTF8         1208
-#define CCSID_ASCII        819
+#define CCSID_JOBDEFAULT               (0)
+#define CCSID_UTF8                     (1208)
+#define CCSID_ASCII                    (819)
+#define CCSID_WINDOWS_1251             (1251)
+#define CCSID_ISO_8859_1               (819)
 
-#define CS_OPER_CSSTRCV    0x00020000
+#define CSSTR_B64_LINEBREAK_OFFSET     (0x0000004D)
+#define CSSTR_B64_MASK_LINEBREAK       (0x0000000F)
+#define CSSTR_B64_LINEBREAK_NONE       (0x00000000)
+#define CSSTR_B64_LINEBREAK_LF         (0x00000001)
+#define CSSTR_B64_LINEBREAK_CRLF       (0x00000002)
+#define CSSTR_B64_IGNOREINVALIDCHAR    (0x00000100)
+
+#define CSSTR_URLENCODE_SPACETOPLUS    (0x00000100)
+#define CSSTR_URLENCODE_XLATERESERVED  (0x00000200)
+
+#define CS_OPER_CSSTRCV                (0x00020000)
 
 typedef void* CSSTRCV;
 
@@ -58,8 +64,8 @@ CSRESULT
 CSRESULT
   CSSTRCV_SetConversion
     (CSSTRCV This,
-     long iFromCCSID,
-     long iToCCSID);
+     char* szFromCCSID,
+     char* szToCCSID);
 
 CSRESULT
   CSSTRCV_StrCpy
@@ -102,11 +108,42 @@ int
     (char* buffer,
      int size);
 
-long
+uint64_t
   CSSTR_ToBase64
-    (char* inBuffer,
-     int   inSize,
-     char* outBuffer,
-     int   outSize);
+    (unsigned char* inBuffer,
+     uint64_t   inSize,
+     unsigned char* outBuffer,
+     uint64_t   outSize);
+
+uint64_t
+  CSSTR_FromBase64Ex
+    (unsigned char* inBuffer,
+     uint64_t   inSize,
+     unsigned char* outBuffer,
+     uint64_t   outSize,
+     int flags);
+
+uint64_t
+  CSSTR_ToBase64Ex
+    (unsigned char* inBuffer,
+     uint64_t   inSize,
+     unsigned char* outBuffer,
+     uint64_t   outSize,
+     int flags);
+
+uint64_t
+  CSSTR_UrlEncode
+    (unsigned char* in,
+     uint64_t inSize,
+     unsigned char* out,
+     uint64_t size,
+     int flags);
+
+uint64_t
+  CSSTR_UrlDecode
+    (unsigned char* in,
+     unsigned char* out,
+     uint64_t size);
+
 
 #endif
